a25e9648405bc11ecf424eea8e6a811dbe727cf7
chrissydileo <Chrissymdean@gmail.com>
2024-07-10 09:52:04 -0400

    Add files via upload

---
+++ PEKDB/js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+    <script>
+        function openExcelFile() {
+            const filePath = "file:///P:/AMZ-PACKAGE/ProvAuto/ProvAuto.xlsm"; // Update this path
+            window.location.href = filePath;
+        }
+    </script>
